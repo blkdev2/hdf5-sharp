ca283ffe44669ef30fb5df1b55e3332b23e243ae
Brian Kauke <kaukeb@gmail.com>
2015-01-16 18:43:04 -0800

    Add dllmap for libdl & attempt to fix FirstTest

--- FirstTest/Main.cs
+++ FirstTest/Main.cs
@@ -29,39 +29,44 @@ namespace Hdf5
             Group g1 = Group.Create(f, "/G1");
             Group g2 = Group.Create(f, "/G2");
             Console.WriteLine("Create file first_test.h5");
-            Dataset ds1, ds2, ds3, ds4, ds5, ds6, ds7, ds8;
+            Dataset<int> ds1, ds2;
+            Dataset<string> ds3, ds4;
+            Dataset<double> ds5;
+            Dataset<Triplet> ds6;
+            //Dataset<ulong> ds7;
+            //Dataset<string> ds8;
 //            ds = Dataset.CreateFromData<int>(f, "T1", new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
             Console.Write("Adding dataset ds1... ");
-            ds1 = Dataset.CreateWithData<int>(f, "T1", new int[,] {{0, 1, 2, 3},
+            ds1 = Dataset<int>.CreateWithData(f, "T1", new int[,] {{0, 1, 2, 3},
                                                                    {1, 2, 3, 4},
                                                                    {2, 3, 4, 5},
                                                                    {3, 4, 5, 6}});
             Console.WriteLine("done.");
             Console.Write("Adding dataset ds2... ");
-            ds2 = Dataset.CreateWithData<double>(f, "T2", new double[,] {{ 0.0,  1.0,  2.0,  3.0},
+            ds2 = Dataset<int>.CreateWithData(f, "T2", new double[,] {{ 0.0,  1.0,  2.0,  3.0},
                                                                          { 4.0,  5.0,  6.0,  7.0},
                                                                          { 8.0,  9.0, 10.0, 11.0},
                                                                          {12.0, 13.0, 14.0, 15.0}});
             Console.WriteLine("done.");
             Console.Write("Adding dataset ds3... ");
-            ds3 = Dataset.CreateWithData(g1, "S1", new string[] {"String 1",
+            ds3 = Dataset<string>.CreateWithData(g1, "S1", new string[] {"String 1",
                                                                  "Some other string",
                                                                  "Third string which is quite a bit longer",
                                                                  "Last!"});
             Console.WriteLine("done.");
             Console.Write("Adding dataset ds4... ");
-            ds4 = Dataset.CreateWithData(g1, "S2", new string[,] {{"S(1,1)", "S(1,2)"},
+            ds4 = Dataset<string>.CreateWithData(g1, "S2", new string[,] {{"S(1,1)", "S(1,2)"},
                                                                   {"S(2,1)", "S(2,2)"}});
             Console.WriteLine("done.");
             Console.Write("Adding dataset ds5... ");
-            ds5 = Dataset.CreateWithData<double>(f, "T3", new double[][] {new double[] { 0.0},
+            ds5 = Dataset<double>.CreateWithData(f, "T3", new double[][] {new double[] { 0.0},
                                                                           new double[] { 1.1,  2.2},
                                                                           new double[] {},
                                                                           new double[] { 3.3,  4.4,  5.5},
                                                                           new double[] { 6.6,  7.7,  8.8,  9.9}});
             Console.WriteLine("done.");
             Console.Write("Adding dataset ds6... ");
-            ds6 = Dataset.CreateWithData<Triplet>(f, "T4", new Triplet[] {new Triplet(0, 0, 5.0),
+            ds6 = Dataset<Triplet>.CreateWithData(f, "T4", new Triplet[] {new Triplet(0, 0, 5.0),
                                                                           new Triplet(0, 1, 3.0),
                                                                           new Triplet(1, 0, 8.0),
                                                                           new Triplet(1, 2, 1.0),
@@ -70,24 +75,25 @@ namespace Hdf5
                                                                           new Triplet(3, 0, 2.0),
                                                                           new Triplet(3, 4, 4.0)});
             Console.WriteLine("done.");
-            Console.Write("Adding dataset ds7... ");
-            ds7 = Dataset.CreateWithData<ulong>(g2, "SC1", 123456);
-            Console.WriteLine("done.");
-            Console.Write("Adding dataset ds8... ");
-            ds8 = Dataset.CreateWithData(g2, "SC2", "scalar string.");
-            Console.WriteLine("done.");
-            Attribute a7, a8;
-            Console.Write("Adding attribute to dataset ds7... ");
-            a7 = Attribute.CreateWithData<sbyte>(ds7, "A7", 1);
-            Console.WriteLine("done.");
-            Console.Write("Adding attribute to dataset ds8... ");
-            a8 = Attribute.CreateWithData(ds8, "A8", "abc");
-            Console.WriteLine("done.");
+            //Console.Write("Adding dataset ds7... ");
+            //ds7 = Dataset<ulong>.CreateWithData(g2, "SC1", 123456);
+            //Console.WriteLine("done.");
+            //Console.Write("Adding dataset ds8... ");
+            //ds8 = Dataset<string>.CreateWithData(g2, "SC2", "scalar string.");
+            //Console.WriteLine("done.");
+            //Attribute<sbyte> a7;
+            //Attribute<string> a8;
+            //Console.Write("Adding attribute to dataset ds7... ");
+            //a7 = Attribute<sbyte>.CreateWithData(ds7, "A7", 1);
+            //Console.WriteLine("done.");
+            //Console.Write("Adding attribute to dataset ds8... ");
+            //a8 = Attribute<string>.CreateWithData(ds8, "A8", "abc");
+            //Console.WriteLine("done.");
             
             Console.WriteLine("\nNumber of objects: {0}", f.NumObjects);
             
             Console.WriteLine("\nds1:");
-            int[,] T1 = (int[,])ds1.ReadValueArray<int>();
+            int[,] T1 = (int[,])ds1.Read();
             for (int i=0; i<T1.GetLength(0); i++)
             {
                 for (int j=0; j<T1.GetLength(1); j++)
@@ -95,7 +101,7 @@ namespace Hdf5
                 Console.WriteLine();
             }
             Console.WriteLine("\nds2:");
-            double[,] T2 = (double[,])ds2.ReadValueArray<double>();
+            double[,] T2 = (double[,])ds2.Read();
             for (int i=0; i<T2.GetLength(0); i++)
             {
                 for (int j=0; j<T2.GetLength(1); j++)
@@ -104,14 +110,14 @@ namespace Hdf5
             }
             
             Console.WriteLine("\nds3:");
-            string[] S1 = (string[])ds3.ReadStringArray();
+            string[] S1 = (string[])ds3.Read();
             for (int i=0; i<S1.GetLength(0); i++)
             {
                 Console.WriteLine(S1[i]);
             }
             
             Console.WriteLine("\nds4:");
-            string[,] S2 = (string[,])ds4.ReadStringArray();
+            string[,] S2 = (string[,])ds4.Read();
             for (int i=0; i<S2.GetLength(0); i++)
             {
                 for (int j=0; j<S2.GetLength(1); j++)
@@ -119,7 +125,7 @@ namespace Hdf5
             }
             
             Console.WriteLine("\nds5:");
-            double[][] T3 = ds5.ReadVariableLength<double>();
+            double[][] T3 = (double[][])ds5.Read();
             for (int i=0; i<T3.Length; i++)
             {
                 for (int j=0; j<T3[i].Length; j++)
@@ -127,25 +133,25 @@ namespace Hdf5
             }
             
             Console.WriteLine("\nds6:");
-            Triplet[] T4 = (Triplet[])ds6.ReadValueArray<Triplet>();
+            Triplet[] T4 = (Triplet[])ds6.Read();
             for (int i=0; i<T4.Length; i++)
             {
                 Console.WriteLine("({0},{1}): {2}", T4[i].i, T4[i].j, T4[i].v);
             }
             
-            Console.WriteLine("\nds7:");
-            ulong SC1 = ds7.ReadValue<ulong>();
-            Console.WriteLine(SC1);
-            Console.WriteLine("\nds7, attribute A7:");
-            sbyte a7b = a7.ReadValue<sbyte>();
-            Console.WriteLine(a7b);
-            
-            Console.WriteLine("\nds8:");
-            string SC2 = ds8.ReadString();
-            Console.WriteLine(SC2);
-            Console.WriteLine("\nds8, attribute A8:");
-            string a8s = a8.ReadString();
-            Console.WriteLine("{0}", a8s);
+//            Console.WriteLine("\nds7:");
+//            //ulong SC1 = ds7.Read();
+//            Console.WriteLine(SC1);
+//            Console.WriteLine("\nds7, attribute A7:");
+//            sbyte a7b = a7.Read();
+//            Console.WriteLine(a7b);
+//            
+//            Console.WriteLine("\nds8:");
+//            string SC2 = ds8.Read();
+//            Console.WriteLine(SC2);
+//            Console.WriteLine("\nds8, attribute A8:");
+//            string a8s = a8.Read();
+//            Console.WriteLine("{0}", a8s);
             
             ds1.Close();
             ds2.Close();
@@ -153,8 +159,8 @@ namespace Hdf5
             ds4.Close();
             ds5.Close();
             ds6.Close();
-            ds7.Close();
-            ds8.Close();
+            //ds7.Close();
+            //ds8.Close();
             g1.Close();
             g2.Close();
             f.Close();
--- hdf5-sharp/Base.cs
+++ hdf5-sharp/Base.cs
@@ -73,19 +73,19 @@ namespace Hdf5
         
         // HDF5 attribute imports
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Acreate2(int obj_id, string attr_name, int type_id, int space_id, int acpl_id, int aapl_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Aclose(int obj_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Aopen_name(int obj_id, string attr_name);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Aread(int attr_id, int mem_type_id, IntPtr buf);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Awrite(int attr_id, int mem_type_id, IntPtr buf);
         
         internal static long H5Aget_name(int attr_id, long size, IntPtr buf)
@@ -93,39 +93,39 @@ namespace Hdf5
             return (long)H5Aget_name(attr_id, (IntPtr)size, buf);
         }
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern IntPtr H5Aget_name(int attr_id, IntPtr size, IntPtr buf);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Aget_space(int attr_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Aget_type(int attr_id);
         
         // HDF5 dataset imports
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dcreate2(int loc_id, string name, int type_id, int space_id, int lcpl_id, int dcpl_id, int dapl_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dopen2(int loc_id, string name, int dapl_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dclose(int dataset_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dget_space(int dataset_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dget_space_status(int dataset_id, out SpaceStatus status);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dget_type(int dataset_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dread(int dataset_id, int mem_type_id, int mem_space_id, int file_space_id, int xfer_plist_id, IntPtr buf);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dwrite(int dataset_id, int mem_type_id, int mem_space_id, int file_space_id, int xfer_plist_id, IntPtr buf);
         
 //        internal static int H5Dvlen_get_buf_size(int dataset_id, int type_id, int space_id, out long size)
@@ -136,27 +136,27 @@ namespace Hdf5
 //            return result;
 //        }
         
-//        [DllImport("libhdf5.so")]
+//        [DllImport("hdf5")]
 //        internal static extern int H5Dvlen_get_buf_size(int dataset_id, int type_id, int space_id, out IntPtr size);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Dvlen_reclaim(int type_id, int space_id, int plist_id, IntPtr buf);
         
         // HDF5 file imports
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Fcreate(string filename, uint flags, int create_id, int access_id);
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Fopen(string filename, uint flags, int access_id);
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Fclose(int file_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Fflush(int loc, FileScope scope);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Fget_filesize(int file_id, out ulong size);
 
         internal static long H5Fget_name(int file_id, IntPtr name, long size)
@@ -164,10 +164,10 @@ namespace Hdf5
             return (long)H5Fget_name(file_id, name, (IntPtr)size);
         }
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern IntPtr H5Fget_name(int file_id, IntPtr name, IntPtr size);
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern IntPtr H5Fget_obj_count(int file_id, uint types);
 
         internal static long H5Fget_obj_ids(int file_id, uint types, long max_objs, IntPtr obj_id_list)
@@ -175,21 +175,21 @@ namespace Hdf5
             return (long)H5Fget_obj_ids(file_id, types, (IntPtr)max_objs, obj_id_list);
         }
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern IntPtr H5Fget_obj_ids(int file_id, uint types, IntPtr max_objs, IntPtr obj_id_list);
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Fis_hdf5(string filename);
         
         // HDF5 group imports
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Gcreate2(int loc, string name, long size_hint, int gcpl_id, int gapl_id);
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Gopen2(int loc, string name, int gapl_id);
 
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Gclose(int group_id);
 
         internal static int H5Gget_num_objs(int loc, out long num_obj)
@@ -200,77 +200,77 @@ namespace Hdf5
             return result;
         }
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Gget_num_objs(int loc, out IntPtr num_obj);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern IntPtr H5Gget_objname_by_idx(int loc, ulong idx, IntPtr name, IntPtr size);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern ObjectType H5Gget_objtype_by_idx(int loc, ulong idx);
         
         // HDF5 dataspace imports
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Screate_simple(int rank, long[] dims, long[] maxdims);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sclose(int space_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sget_simple_extent_ndims(int space_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sget_simple_extent_dims(int space_id, IntPtr dims, IntPtr maxdims);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sis_simple(int space_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sselect_all(int space_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sselect_none(int space_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sselect_hyperslab(int space_id, SelectOperation op, IntPtr start, IntPtr stride, IntPtr count, IntPtr block);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Sselect_valid(int space_id);
         
         // HDF5 datatype imports
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tcreate(DatatypeClass klass, IntPtr size);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tvlen_create(int base_type_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tcopy(int type_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tinsert(int type_id, string name, IntPtr offset, int field_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tclose(int type_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern DatatypeClass H5Tget_class(int type_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern IntPtr H5Tget_size(int type_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tset_size(int type_id, long size);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern ByteOrder H5Tget_order(int type_id);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tset_order(int type_id, ByteOrder order);
         
-        [DllImport("libhdf5.so")]
+        [DllImport("hdf5")]
         internal static extern int H5Tis_variable_str(int type_id);
     }
 }
